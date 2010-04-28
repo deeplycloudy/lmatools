dd45575d537e279409e72b0ceea94a34e5c0a564
Username Eric <eric.bruning@gmail.com>
2010-04-28 10:17:26 -0400

    Tweaked output filename template to indicate minimum number of sources per flash

--- make_grids.py
+++ make_grids.py
@@ -252,7 +252,7 @@ def grid_h5flashfiles(h5_filenames, start_time, end_time,
     lons.shape=x_all.shape
     lats.shape=y_all.shape
     
-    outflile_basename = os.path.join(outpath,'LMA_%s_%d_' % (start_time.strftime('%Y%m%d_%H%M%S'), to_seconds(duration)))
+    outflile_basename = os.path.join(outpath,'LMA_%s_%d_%dsrc_' % (start_time.strftime('%Y%m%d_%H%M%S'), to_seconds(duration), min_points_per_flash))
     
     outfiles = (outflile_basename+'flash_extent.nc',
                 outflile_basename+'flash_init.nc',
