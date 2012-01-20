6531590a35d3ae453dd17640335ddff1c46fbe9c
Username Eric <eric.bruning@gmail.com>
2012-01-20 13:11:23 -0700

    More packaging fixes - setup.py automated package finding

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(name='lmatools',
     version='0.1',
@@ -6,7 +6,8 @@ setup(name='lmatools',
     author='Eric Bruning',
     author_email='eric.bruning@gmail.com',
     url='https://bitbucket.org/deeplycloudy/lmatools/',
-    packages=['flashsort', 'flashsort.autosort'],
-    py_modules=['coordinateSystems', 'density_to_files', 'density_tools', 
+    packages = find_packages()
+#    packages=['flashsort', 'flashsort.autosort'],
+#    py_modules=['coordinateSystems', 'density_to_files', 'density_tools', 
                 'make_grids', 'multiples_nc', 'multiples', 'small_multiples'],
     )
