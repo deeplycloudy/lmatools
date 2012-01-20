49d71e99f02a0e1f706027e9d4fa92282b018623
Username Eric <eric.bruning@gmail.com>
2012-01-20 13:19:37 -0700

    One more try with setup.py foolery.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='lmatools',
     author_email='eric.bruning@gmail.com',
     url='https://bitbucket.org/deeplycloudy/lmatools/',
     packages = find_packages(),
-#    packages=['flashsort', 'flashsort.autosort'],
+    packages=['flashsort', 'flashsort.autosort'],
 #    py_modules=['coordinateSystems', 'density_to_files', 'density_tools', 
 #                'make_grids', 'multiples_nc', 'multiples', 'small_multiples'],
     )
