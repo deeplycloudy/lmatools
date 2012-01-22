f3feaf9b51b32d10bfa123ddb58d4a220b3bebbd
Username Eric <eric.bruning@gmail.com>
2012-01-22 11:06:39 -0600

    Fix setup.py script to create correct lmatools package and subpackages

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='lmatools',
     author='Eric Bruning',
     author_email='eric.bruning@gmail.com',
     url='https://bitbucket.org/deeplycloudy/lmatools/',
-    packages=['flashsort'],
-    py_modules=['coordinateSystems', 'density_to_files', 'density_tools', 
-                'make_grids', 'multiples_nc', 'multiples', 'small_multiples'],
+    package_dir={'lmatools': ''}, # wouldn't be necessary if we reorganized to traditional package layout with lmatools at the same directory level as the setup.py script.
+    packages=['lmatools', 'lmatools.flashsort', 'lmatools.flashsort.autosort'],
     )
