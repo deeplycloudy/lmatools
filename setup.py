2619c770a6f7aa33760928971a08811d7313eb5c
Username Eric <eric.bruning@gmail.com>
2012-01-26 12:54:50 -0600

    Fixing setup.py one last time

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, find_packages
+from distutils.core import setup
 
 setup(name='lmatools',
     version='0.1',
@@ -6,13 +6,6 @@ setup(name='lmatools',
     author='Eric Bruning',
     author_email='eric.bruning@gmail.com',
     url='https://bitbucket.org/deeplycloudy/lmatools/',
-<<<<<<< local
     package_dir={'lmatools': ''}, # wouldn't be necessary if we reorganized to traditional package layout with lmatools at the same directory level as the setup.py script.
     packages=['lmatools', 'lmatools.flashsort', 'lmatools.flashsort.autosort'],
-    )=======
-    packages = find_packages(),
-    packages=['flashsort', 'flashsort.autosort'],
-#    py_modules=['coordinateSystems', 'density_to_files', 'density_tools', 
-#                'make_grids', 'multiples_nc', 'multiples', 'small_multiples'],
     )
->>>>>>> other
