d5569f0cf0317e89a6c4d7180b3d9c4e38af1047
Username Eric <eric.bruning@gmail.com>
2013-11-20 09:24:57 -0600

    Factor out h5 flash file reader into new io module

--- make_grids.py
+++ make_grids.py
@@ -6,13 +6,9 @@ import numpy as np
 import tables
 
 import density_to_files
+from lmatools.io import read_flashes, to_seconds
 
 from coordinateSystems import MapProjection, GeographicSystem
-
-
-def to_seconds(dt):
-    'convert datetime.timedelta object to float seconds'
-    return dt.days * 86400 + dt.seconds + dt.microseconds/1.0e6
     
 
 def dlonlat_at_grid_center(ctr_lat, ctr_lon, dx=4.0e3, dy=4.0e3,
@@ -141,50 +137,6 @@ def write_cf_netcdf(outfile, t_start, t, xloc, yloc, lon_for_x, lat_for_y, ctr_l
 
 
 
-def read_flashes(h5LMAfiles, target, base_date=None, min_points=10):
-    """ This routine is the data pipeline source, responsible for pushing out 
-        events and flashes. Using a different flash data source format is a matter of
-        replacing this routine to read in the necessary event and flash data."""
-
-    for filename in h5LMAfiles:
-        h5 = tables.openFile(filename)
-        table_name = h5.root.events._v_children.keys()[0]
-
-        print filename
-
-        # event_dtype = getattr(h5.root.events, table_name)[0].dtype
-        # events_all = getattr(h5.root.events, table_name)[:]
-        flashes = getattr(h5.root.flashes, table_name)
-        events = getattr(h5.root.events, table_name)[:]
-        
-        if flashes.shape[0] == 0:
-            # There are no flashes
-            pass
-        
-        fl_dtype = flashes.dtype
-        flashes = np.fromiter((fl[:] for fl in flashes if fl['n_points'] >= min_points), dtype=fl_dtype)
-
-        # get the start date of the file, which is the seconds-of-day reference
-        h5_start =  datetime(*getattr(h5.root.events, table_name).attrs['start_time'][0:3])
-        if base_date==None:
-            base_date = h5_start
-        extra_dt = to_seconds(h5_start - base_date)
-        # adjust time in seconds of day to be in correct reference
-        events['time'] += extra_dt
-        flashes['start'] += extra_dt
-
-        n_flashes = len(flashes)
-        print '    ', n_flashes, ' total flashes, with extra dt of', extra_dt
-        
-        push_out = (events, flashes)
-
-        target.send(push_out)
-        
-        # del events
-        # del flashes
-        del push_out
-
-        h5.close()
         
 
 def grid_h5flashfiles(h5_filenames, start_time, end_time, 
