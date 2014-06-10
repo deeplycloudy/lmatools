0e98ff5c5498f415ab2827e57d0088da03094551
Eric <eric.bruning@gmail.com>
2014-06-10 15:27:58 -0500

    Added flash volume calculation. Removed unused kwarg for max_pts.

--- flashsort/autosort/flash_stats.py
+++ flashsort/autosort/flash_stats.py
@@ -56,7 +56,35 @@ def poly_area(x,y):
     area *= 0.5
     return area
     
-def calculate_flash_stats(flash, min_pts=2, max_pts=10):
+
+def hull_volume(xyz):
+    """ Calculate the spatial volume of the convex hull of 3D LMA data.
+        xyzt is a (N_points, 3) array of point locations in space. """
+    assert xyz.shape[1] == 3
+    
+    from scipy.spatial import Delaunay
+    from scipy.misc import factorial
+
+    tri = Delaunay(xyz[:,0:3])
+    vertices = tri.points[tri.vertices]
+
+    # This is the volume formula in 
+    # https://github.com/scipy/scipy/blob/master/scipy/spatial/tests/test_qhull.py#L106
+    # Except the formula needs to be divided by ndim! to get the volume, cf., 
+    # http://en.wikipedia.org/wiki/Simplex#Geometric_properties
+    # Credit Pauli Virtanen, Oct 14, 2012, scipy-user list
+
+    q = vertices[:,:-1,:] - vertices[:,-1,None,:]
+    simplex_volumes = (1.0 / factorial(q.shape[-1])) * np.fromiter(
+           (np.linalg.det(q[k,:,:]) for k in range(tri.nsimplex)) , dtype=float)
+
+    # The simplex volumes have negative values since they are oriented 
+    # (think surface normal direction for a triangle
+    volume=np.sum(np.abs(simplex_volumes))
+    return volume
+
+    
+def calculate_flash_stats(flash, min_pts=2):
     logger = logging.getLogger('FlashAutorunLogger')
     
     Re = 6378.137*1000;           #Earth's radius in m
@@ -67,7 +95,7 @@ def calculate_flash_stats(flash, min_pts=2, max_pts=10):
     fl_id = np.unique(flash.points['flash_id'])
     assert (fl_id.shape[0] == 1)
     flash.id = fl_id[0]
-
+    
     
     lat = np.asarray(flash.points['lat'],dtype=float) 
     lon = np.asarray(flash.points['lon'],dtype=float) 
@@ -104,6 +132,11 @@ def calculate_flash_stats(flash, min_pts=2, max_pts=10):
             logger.warning('Setting area to 0 for flash with points %s, %s' % (x, y))
             area=0.0
             
+    volume = 0.0
+    if flash.pointCount > 3:
+        # Need four points to make at least one tetrahedron.
+        volume = hull_volume(np.vstack((x,y,z)).T)
+            
     flash.start   = flash.points['time'].min()
     flash.end     = flash.points['time'].max()
     flash.duration = flash.end - flash.start
@@ -116,3 +149,4 @@ def calculate_flash_stats(flash, min_pts=2, max_pts=10):
     flash.ctralt  = altavg
     flash.ctrlat  = latavg
     flash.ctrlon  = lonavg
+    flash.volume  = volume / 1.0e9 # km^3, 1000x1000x1000 m
