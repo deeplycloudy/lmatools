fa142a4905d32a1b74549b01b620b39689db1fbb
Eric Bruning <eric.bruning@gmail.com>
2020-04-28 16:44:47 -0500

    Fix error on single-location coords that triggered a pyproj error

--- lmatools/coordinateSystems.py
+++ lmatools/coordinateSystems.py
@@ -71,6 +71,9 @@ class GeographicSystem(CoordinateSystem):
             self.ERSlla = proj4.Proj(proj='latlong', ellps=ellipse, datum=datum)
         self.ERSxyz = proj4.Proj(proj='geocent', ellps=ellipse, datum=datum)
     def toECEF(self, lon, lat, alt):
+        lat = atleast_1d(lat)
+        lon = atleast_1d(lon)
+        alt = atleast_1d(alt)
         projectedData = array(proj4.transform(self.ERSlla, self.ERSxyz, lon, lat, alt ))
         if len(projectedData.shape) == 1:
             return projectedData[0], projectedData[1], projectedData[2]
@@ -78,6 +81,9 @@ class GeographicSystem(CoordinateSystem):
             return projectedData[0,:], projectedData[1,:], projectedData[2,:]
 
     def fromECEF(self, x, y, z):
+        x = atleast_1d(x)
+        y = atleast_1d(y)
+        z = atleast_1d(z)
         projectedData = array(proj4.transform(self.ERSxyz, self.ERSlla, x, y, z ))
         if len(projectedData.shape) == 1:
             return projectedData[0], projectedData[1], projectedData[2]
